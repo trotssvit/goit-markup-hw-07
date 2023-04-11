ae7f1ca8d434de7d700327103cf84c69d88d16d8
Svitlana Trotsenko <trots.svit@gmail.com>
2023-04-12 00:25:24 +0100

    mobile menu
    mobile menu without horis scroll

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector(".menu-mob-btn-open"),
+      closeMenuBtn: document.querySelector(".menu-mob-btn-close"),
+      menu: document.querySelector(".menu-mobile"),
+      body: document.querySelector("body"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
+    }
+  })();
